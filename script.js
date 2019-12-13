0c5c9a122a3d89d95c0836ad3b6fc15ea9f5298b
chrisp-dev <chris.paradeise@gmail.com>
2019-12-13 10:37:46 -0800

    Added text to index.html

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
 
-    // rapidapi drinkanator key 
+    // variables
     // https://rapidapi.com/developer/security/Drinkanator
     const cocktailKey = 'f5fa4c0484mshad6cb57c6f05a3fp195dcejsn6e3f9016299c'
-    const cocktailApiUrl = "https://the-cocktail-db.p.rapidapi.com/random.php";
+    const cocktailApiUrl = "https://the-cocktail-db.p.rapidapi.com/random.php?apiid=" + cocktailKey;
     const drizlyApiUrl = '';
+
     /**
      * get data from api
      */
     function getData(apiUrl, queryString) {
-        let dataUrl = cocktailApiUrl; //apiUrl + "q=" + queryString;
+        let dataUrl = apiUrl + "q=" + queryString;
 
         $.ajax({
             url: dataUrl,
@@ -22,5 +23,11 @@ $(document).ready(function () {
         });
     }
 
-    getData();
+    function renderDrinkInfo(data) {
+        // idDrink
+        // strAlcoholic
+    }
+
+    // TODO: move this to an event handler
+    getData(cocktailApiUrl, 'vodka');
 });
