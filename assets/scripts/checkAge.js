21fbd82cf747e30296e9022dabadceea54971319
Chris Paradeise <chris.paradeise@gmail.com>
2019-12-17 20:09:57 -0800

    quuik

--- assets/scripts/checkAge.js
+++ assets/scripts/checkAge.js
@@ -4,7 +4,7 @@ let deployed = (window.location.host === "chrisp-dev.github.io");
 let localFileOrigin = window.location.origin === 'file://';
 let prepath = deployed ? '/drinkanator/' : '';
 if (!userOfAge && (!localFileOrigin && window.location.pathname !== prepath && window.location.pathname !== (prepath + 'index.html'))) {
-    window.location.href = window.location.host + prepath + 'index.html';
+    window.location.href = prepath + 'index.html';
 } else if (userOfAge && window.location.pathname === (prepath + 'index.html')) {
     document.querySelector('.checkAge-wrapper').style.display = 'none';
     document.querySelector('.black-trans-bkg').style.display = 'none';
